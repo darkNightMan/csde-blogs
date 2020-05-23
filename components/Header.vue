b6effd1b9894d1cf4cd6f46f73d0162532ff6040
wangxiping@banggood.com <wangxiping@banggood.com>
2020-05-23 11:57:48 +0800

    更新项目

---
+++ components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <mu-appbar style="width: 100%;" color="primary">
+    <!-- <mu-button icon slot="left">
+      <mu-icon value="menu"></mu-icon>
+    </mu-button> -->
+    <mu-row>
+      <mu-col span="6">CSDE.W</mu-col>
+      <mu-col span="5">
+           <mu-row class="header-nav">
+              <mu-col span="2">首页</mu-col>
+              <mu-col span="2">博客</mu-col>
+              <mu-col span="2">归档</mu-col>
+              <mu-col span="2">关于</mu-col>
+              <mu-col span="2">友链</mu-col>
+              <mu-col span="2">留言</mu-col>
+          </mu-row>
+      </mu-col>
+    </mu-row>
+  </mu-appbar>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header-nav{
+  font-size: 14px;
+  cursor: pointer;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <Header></Header>
+</template>
+
+<script>
+import Header from '../components/Header'
+export default {
+  components: {
+    Header
+  },
+  data () {
+    return {
+      active: 0
+    };
+  }
+}
+</script>
+<style lang="less">
+.demo-text {
+  padding: 16px;
+  background: #fff;
+  p {
+    margin: 8px 0;
+  }
+}
+.icon-flex-wrap .mu-button {
+  margin: 6px 8px;
+}
+</style>
